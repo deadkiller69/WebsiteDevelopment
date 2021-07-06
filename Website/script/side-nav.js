79532ee911e6a625024ce2111e0a35a0d3179569
Ankit Sharma <66247074+deadkiller69@users.noreply.github.com>
2021-07-06 13:55:43 +0545

    Add files via upload

---
+++ Website/script/side-nav.js
@@ -0,0 +1,8 @@
+
+      function openNav() {
+        document.getElementById("mySidenav").style.width = "99.80%";
+      }
+      
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0%";
+      }
